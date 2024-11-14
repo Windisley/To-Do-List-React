cfea7ffe24e97a265e7b47853d5a15a76b4c1536
Windisley <windisleyl@gmail.com>
2024-11-14 00:43:36 -0300

    To-Do List Estruturada e Estilizada com Tailwind

---
+++ todo_list/todo-list-new/src/App.jsx
@@ -0,0 +1,102 @@
+import { useState, useRef, useEffect } from 'react'
+import { AiFillDelete } from "react-icons/ai";
+import { MdDataSaverOn } from "react-icons/md";
+
+
+
+
+function App() {
+  const [list, setList] = useState([])
+  const $inputref = useRef()
+
+
+
+
+  function handlevalue(){
+   const newtask = $inputref.current.value
+   
+   if(newtask){
+     setList((lists)=> [...lists, newtask])  
+     $inputref.current.value = ""
+
+
+   }else{
+     alert("Campo Vazio Digite Uma Tarefa!")
+   }
+ 
+   console.log(list)
+  }
+
+
+  function delitem(index){
+
+
+    setList((prevList)=>{
+        const updatedList = prevList.filter((_, i) => i !== index);
+        localStorage.setItem("taskList", JSON.stringify(updatedList));
+        return updatedList;
+
+
+    })
+   
+  }
+
+
+  function savelist() {
+    localStorage.setItem("taskList", JSON.stringify(list));
+   
+  alert("Tasks saved!");
+  }
+
+
+useEffect(() => {
+   
+const savedList = localStorage.getItem("taskList");
+   
+ 
+if (savedList) {
+     
+setList(JSON.parse(savedList));
+  }
+}, []);
+
+
+
+
+  return (
+    <div className='bg-background w-full  min-h-dvh flex justify-center items-center flex-col pt-6 pb-6'>
+        <div className=' max-w-full min-h-96 border-2 border-secundary p-6 rounded-xl flex flex-col gap-y-8'>
+            <div className='flex justify-center gap-8'>
+               <input ref={$inputref}  type="text" placeholder='Digite uma tarefa!' className='w-full max-w-80 placeholder:text-xs px-12 py-1 border-2 text-primary text-center outline-none border-secundary bg-secundary'/>
+               <button onClick={handlevalue} className='w-full  max-w-52 px-8 border-2 border-secundary bg-secundary text-primary capitalize hover:scale-110 ease-in-out duration-300 rounded-lg'>enviar</button>
+            </div>
+
+
+            <div className='flex flex-col justify-center items-center w-full max-w-5xl'>
+              {list &&
+              list.map((iten, index)=>(
+                <div  key={index} className='w-full  max-w-full   p-2 border-2 border-gray-300 border-b-1 flex justify-center items-center relative'>
+                   <ul className='truncate'>
+                     <li className='text-secundary  capitalize text-sm'>
+                        {iten}
+                     </li>
+                   </ul>
+                  <div onClick={()=>{delitem(index)}} className='absolute bottom-1 mx-2 translate-y-1/2 left-full  cursor-pointer'>
+                    <AiFillDelete className='hover:text-red-600 ease-in-out duration-300  text-size-4'/>
+                  </div>
+                  <div onClick={()=>{savelist()}} className='absolute top-0 mx-2 translate-y-1/2 left-full cursor-pointer'>
+                     <MdDataSaverOn className='hover:text-green-500 ease-in-out duration-300  text-size-4'/>
+                  </div>
+                </div>
+              ))
+               
+              }
+           </div>
+        </div>
+       
+    </div>
+  )
+}
+
+
+export default App
